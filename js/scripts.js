6805f26eaa4b79b6802251920e1bda48cf0eb3f0
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-01-13 09:41:18 -0800

    It will add start button for Title Section, add click event for start button.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+  //Front Logic Goes Here
+
+  // Title Button
+  $("#title button").click(function(event){
+    event.preventDefault();
+    $("#question1").slideToggle();
+  });
+
+});
